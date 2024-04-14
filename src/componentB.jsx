82e6678836286f87331a33a25a8df4278819b38c
mansibansal9648 <mansibansal12345@gmail.com>
2024-04-14 22:39:47 +0530

    usage of useContext instead of consumer

--- src/componentB.jsx
+++ src/componentB.jsx
@@ -1,7 +1,15 @@
 import ComponentC from "./componentC";
+import { firstName, lastName } from "./App";
+import { useContext } from "react";
+
 function ComponentB() {
+  const fname = useContext(firstName);
+  const lname = useContext(lastName);
   return (
     <>
+      <h1>
+        Welcome developer {fname} {lname}
+      </h1>
       <ComponentC />
     </>
   );
